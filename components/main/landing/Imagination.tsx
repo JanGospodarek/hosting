94969b9e60d2e6bdc8304d690d5351682e864c33
JanGospodarek <jan.gospo@onet.pl>
2023-08-17 18:16:06 +0200

    imagination

--- components/main/landing/Imagination.tsx
+++ components/main/landing/Imagination.tsx
@@ -1,9 +1,30 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFountainPreset } from "tsparticles-preset-fountain";
+
 const Imagination = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFountainPreset(engine);
+  }, []);
+  const options = {
+    preset: "fountain",
+    fullScreen: { enable: false, zIndex: 0 },
+  };
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
   return (
-    <div className="h-screen bg-base-300 flex items-center justify-center">
-      <div className="flex flex-col items-center">
-        <p className="text-5xl font-semibold">Puść wodzę wyobraźni</p>
-        <p className="text-2xl mt-2">A my przeniesiemy ją do internetu</p>
+    <div className="h-screen bg-base-300 flex items-center justify-center relative">
+      <div className="absolute w-full h-full  ">
+        <Particles
+          options={options}
+          init={particlesInit}
+          className="absolute w-full h-full  "
+        />
+        <div className="flex flex-col items-center absolute left-0 right-0 mt-64 m-auto z-10">
+          <p className="text-6xl font-semibold">Puść wodzę fantazji</p>
+          <p className="text-2xl mt-2">A my przeniesiemy ją do internetu</p>
+        </div>
       </div>
     </div>
   );
