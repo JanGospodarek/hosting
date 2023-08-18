cee9740647a3a628977a5d0cc6c45ad5f52f4fbe
JanGospodarek <jan.gospo@onet.pl>
2023-08-18 18:49:55 +0200

    form

--- components/main/landing/Contact.tsx
+++ components/main/landing/Contact.tsx
@@ -1,6 +1,49 @@
 import Lottie from "lottie-react";
 import ContactLottie from "../../../public/lottie/contact.json";
+import { useRef, useState } from "react";
+import { sendContactForm } from "../../../lib/api";
 const Contact = () => {
+  const [nameErr, setNameErr] = useState(false);
+  const [lastNameErr, setLastNameErr] = useState(false);
+  const [emailErr, setEmailErr] = useState(false);
+  const [descriptionErr, setDescriptionErr] = useState(false);
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const lastNameRef = useRef<HTMLInputElement | null>(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const companyRef = useRef<HTMLInputElement | null>(null);
+  const descRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleSendForm = async () => {
+    const name = nameRef.current.value.trim();
+    const lastName = lastNameRef.current.value.trim();
+    const email = emailRef.current.value.trim();
+    const description = descRef.current.value.trim();
+    const company = companyRef.current.value.trim();
+
+    name === "" ? setNameErr(true) : setNameErr(false);
+    lastName === "" ? setLastNameErr(true) : setLastNameErr(false);
+    email === "" ? setEmailErr(true) : setEmailErr(false);
+    description == "" ? setDescriptionErr(true) : setDescriptionErr(false);
+
+    if (name !== "" || lastName !== "" || email !== "" || description !== "") {
+      // handle submition
+      console.log("dziala!!!");
+      // loading
+      try {
+        await sendContactForm({ name, lastName, email, description, company });
+        // setTouched({});
+        // setState(initState);
+        // toast({
+        //   title: "Message sent.",
+        //   status: "success",
+        //   duration: 2000,
+        //   position: "top",
+        // });
+      } catch (error) {
+        // turn off loading and siplay error
+      }
+    }
+  };
   return (
     <div className="rounded-lg shadow-xl md:w-3/4 lg:w-[800px] mx-auto mt-24 p-8">
       <div className="flex">
@@ -10,18 +53,33 @@ const Contact = () => {
           <input
             type="text"
             placeholder="Imię"
-            className="input input-bordered w-full max-w-xs m-3"
+            className={`input input-bordered w-full max-w-xs m-3 ${
+              nameErr && "input-error"
+            }`}
+            ref={nameRef}
           />
           <input
             type="text"
             placeholder="Nazwisko"
-            className="input input-bordered w-full max-w-xs m-3"
+            className={`input input-bordered w-full max-w-xs m-3 ${
+              lastNameErr && "input-error"
+            }`}
+            ref={lastNameRef}
           />
           <input
             type="email"
             placeholder="Email"
             required
+            className={`input input-bordered w-full max-w-xs m-3 ${
+              emailErr && "input-error"
+            }`}
+            ref={emailRef}
+          />
+          <input
+            type="text"
+            placeholder="Nazwa twojej firmy (opcjonalne)"
             className="input input-bordered w-full max-w-xs mx-3 mt-3"
+            ref={companyRef}
           />
         </div>
         <Lottie
@@ -37,12 +95,17 @@ const Contact = () => {
           <span className="label-text">Opis projektu</span>
         </label>
         <textarea
-          className="textarea textarea-bordered h-24"
           placeholder="Opisz swój projekt, na przykład: masz moze jakąś wizję, czym się zajmuje twoja firma, co chcesz by się znajdowało na stronie"
+          ref={descRef}
+          className={`textarea textarea-bordered  h-24 ${
+            descriptionErr && "textarea-error"
+          }`}
         ></textarea>
       </div>
 
-      <button className="btn btn-success m-4">Wyślij</button>
+      <button className="btn btn-success m-4" onClick={handleSendForm}>
+        Wyślij
+      </button>
     </div>
   );
 };
