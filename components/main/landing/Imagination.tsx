878423883131599995c84465fee91f2f257a3c53
JanGospodarek <jan.gospo@onet.pl>
2023-08-19 23:06:06 +0200

    mobile device bg

--- components/main/landing/Imagination.tsx
+++ components/main/landing/Imagination.tsx
@@ -1,7 +1,8 @@
 import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFountainPreset } from "tsparticles-preset-fountain";
-
+import Image from "next/image";
+import Bg from "../../../public/img/imagination-bg.png";
 const Imagination = () => {
   const particlesInit = useCallback(async (engine) => {
     await loadFountainPreset(engine);
@@ -12,16 +13,21 @@ const Imagination = () => {
   };
 
   return (
-    <div className="h-screen bg-base-300 flex items-center justify-center relative">
+    <div className="h-screen  flex items-center justify-center relative">
       <div className="absolute w-full h-full  ">
         <Particles
           options={options}
           init={particlesInit}
-          className="absolute w-full h-full  "
+          className="absolute w-full h-full hidden sm:block "
         />
+        <Image
+          src={Bg}
+          alt="bg"
+          className="absolute h-[800px] block sm:hidden "
+        ></Image>
         <div className="flex flex-col items-center absolute left-0 right-0 mt-64 m-auto z-10">
           <p className="text-6xl font-semibold text-center">
-            Puść wodzę fantazji
+            Puść wodze fantazji
           </p>
           <p className="text-2xl mt-2 text-slate-600">
             A my przeniesiemy ją do internetu
