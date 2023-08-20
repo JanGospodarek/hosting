7fe5d54c9c307a8c5b7af06f3446086c429c5e1d
JanGospodarek <jan.gospo@onet.pl>
2023-08-20 14:35:20 +0200

    imagination animation change

--- components/main/landing/Imagination.tsx
+++ components/main/landing/Imagination.tsx
@@ -3,6 +3,8 @@ import Particles from "react-tsparticles";
 import { loadFountainPreset } from "tsparticles-preset-fountain";
 import Image from "next/image";
 import Bg from "../../../public/img/imagination-bg.png";
+import Lottie from "lottie-react";
+import ImaginationLottie from "../../../public/lottie/imagination.json";
 const Imagination = () => {
   const particlesInit = useCallback(async (engine) => {
     await loadFountainPreset(engine);
@@ -14,22 +16,27 @@ const Imagination = () => {
 
   return (
     <div className="h-screen  flex items-center justify-center relative">
-      <div className="absolute w-full h-full  ">
-        <Particles
-          options={options}
-          init={particlesInit}
-          className="absolute w-full h-full "
-        />
+      <div className="absolute w-full h-full   ">
         {/* <Particles
           options={options}
           init={particlesInit}
-          className="absolute w-full h-full hidden sm:block "
+          className="absolute w-full h-full "
         /> */}
+        <Particles
+          options={options}
+          init={particlesInit}
+          className="absolute w-full h-full hidden sm:block "
+        />
         {/* <Image
           src={Bg}
           alt="bg"
           className="absolute h-[800px] block sm:hidden "
         ></Image> */}
+        <Lottie
+          animationData={ImaginationLottie}
+          loop={true}
+          className="mx-auto absolute h-[800px] block sm:hidden"
+        ></Lottie>
         <div className="flex flex-col items-center absolute left-0 right-0 mt-64 m-auto z-10">
           <p className="text-6xl font-semibold text-center">
             Puść wodze fantazji
--- components/main/landing/SingleBenefit.tsx
+++ components/main/landing/SingleBenefit.tsx
@@ -10,13 +10,13 @@ const SingleBenefit = (props: {
   const { title, body, placementClass, lottieData, animationWidth } = props;
   return (
     <div
-      className={`flex sm:mx-16 lg:mx-0 lg:w-1/2  my-4 rounded-xl shadow-2xl text-center p-6 h-64 ${
+      className={`flex sm:mx-16 lg:mx-0 lg:w-1/2  my-4 rounded-xl shadow-2xl text-center p-6 h-56 ${
         placementClass ? placementClass : ""
       }`}
     >
-      <div className="flex flex-col mt-6">
+      <div className="flex flex-col my-auto">
         <p className="text-2xl font-semibold">{title}</p>
-        <p className="text-lg text-slate-600">{body}</p>
+        <p className="text-lg text-slate-600 mt-2">{body}</p>
       </div>
       <Lottie
         animationData={lottieData}
