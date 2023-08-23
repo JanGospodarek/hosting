315978fed5b727251e935a8edae833b75a3df210
JanGospodarek <jan.gospo@onet.pl>
2023-08-23 23:28:59 +0200

    dd

--- components/main/landing/Imagination.tsx
+++ components/main/landing/Imagination.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import Particles from "react-tsparticles";
 import { loadFountainPreset } from "tsparticles-preset-fountain";
 import { Link } from "react-scroll";
@@ -12,48 +12,46 @@ const Imagination = () => {
   //   preset: "fountain",
   //   fullScreen: { enable: false, zIndex: 0 },
   // };
-
   return (
-    <div className="h-screen  flex items-center justify-center relative">
-      <div className="absolute w-full h-full   ">
-        {/* <Particles
+    <div className="h-screen  flex flex-col justify-center items-center  ">
+      {/* <Particles
           options={options}
           init={particlesInit}
           className="absolute w-full h-full "
         /> */}
-        {/* <Particles
+      {/* <Particles
           options={options}
           init={particlesInit}
           className="absolute w-full h-full hidden sm:block "
         /> */}
-        {/* <Image
+      {/* <Image
           src={Bg}
           alt="bg"
           className="absolute h-[800px] block sm:hidden "
         ></Image> */}
-        <Lottie
-          animationData={ImaginationLottie}
-          loop={true}
-          className=" absolute h-[600px] left-0 right-0 "
-        ></Lottie>
-        <div className="flex flex-col items-center absolute left-0 right-0 mt-64 m-auto z-10">
-          <p className="text-6xl font-semibold text-center">
-            Puść wodze fantazji
-          </p>
-          <p className="text-2xl mt-2 text-slate-600">
-            A my przeniesiemy ją do internetu
-          </p>
-          {/* <Link
-            to="example_projects"
-            smooth={true}
-            offset={-100}
-            duration={1500}
-            className="btn btn-outline mt-40 btn-lg"
-          >
-            Projekty
-          </Link> */}
-        </div>
+
+      <div className="flex flex-col items-center  mt-24  ">
+        <p className="text-6xl font-semibold text-center">
+          Puść wodze fantazji
+        </p>
+        <p className="text-2xl mt-2 text-slate-600">
+          A my przeniesiemy ją do internetu
+        </p>
+        <Link
+          to="example_projects"
+          smooth={true}
+          offset={-100}
+          duration={1500}
+          className="btn btn-outline mt-4 btn-lg"
+        >
+          Projekty
+        </Link>
       </div>
+      <Lottie
+        animationData={ImaginationLottie}
+        loop={true}
+        className="  h-[400px] xl:h-[500px] "
+      ></Lottie>
     </div>
   );
 };
