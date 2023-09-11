35eb685d3c954f56b5b4e6369f738ff567257a54
JanGospodarek <jan.gospo@onet.pl>
2023-09-11 12:03:45 +0200

    fix scaling img

--- components/main/landing/SingleExampleProject.tsx
+++ components/main/landing/SingleExampleProject.tsx
@@ -9,13 +9,13 @@ const SingleExampleProject = (props: {
   const { websiteUrl, hrefUrl, image, children } = props;
   return (
     <Link href={hrefUrl}>
-      <div className=" mx-16 md:mx-40 xl:mx-64 rounded-xl border-b shadow-xl my-8 ">
+      <div className="scale-90 sm:scale-100 mx-16 md:mx-40 xl:mx-64 rounded-xl border-b shadow-xl my-8 ">
         <div className="mockup-browser border bg-base-300 ">
           <div className="mockup-browser-toolbar">
             <div className="input">{websiteUrl}</div>
           </div>
           <div className="flex justify-center px-4 py-16 bg-base-200">
-            <Image src={image} alt="coffe" className="  h-full"></Image>
+            <Image src={image} alt="coffe" className="w-full"></Image>
           </div>
         </div>
         <div className="my-4 mx-12">{children}</div>
