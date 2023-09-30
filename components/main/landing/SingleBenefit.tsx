efe0116ec36c2d1e67cb6149609e5f711526686c
JanGospodarek <jan.gospo@onet.pl>
2023-09-30 10:58:22 +0200

    ddd

--- components/main/landing/SingleBenefit.tsx
+++ components/main/landing/SingleBenefit.tsx
@@ -1,4 +1,5 @@
 import Lottie from "lottie-react";
+import { Fade, Slide } from "react-awesome-reveal";
 
 const SingleBenefit = (props: {
   title: string;
@@ -9,22 +10,26 @@ const SingleBenefit = (props: {
 }) => {
   const { title, body, placementClass, lottieData, animationWidth } = props;
   return (
-    <div
-      className={`flex sm:mx-16 lg:mx-0 flex-col md:flex-row lg:w-1/2  my-4 rounded-xl shadow-2xl text-center p-6 h-auto sm:h-56 ${
-        placementClass ? placementClass : ""
-      }`}
-    >
-      <div className="flex flex-col my-auto">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="text-lg text-slate-600 mt-2">{body}</p>
-      </div>
-      <Lottie
-        animationData={lottieData}
-        loop={true}
-        // style={{ width: animationWidth }}
-        className="mx-auto w-48"
-      />
-    </div>
+    <Fade>
+      <Slide direction="up">
+        <div
+          className={`flex sm:mx-16 lg:mx-0 flex-col md:flex-row lg:w-1/2  my-4 rounded-xl shadow-2xl text-center p-6 h-auto sm:h-56 ${
+            placementClass ? placementClass : ""
+          }`}
+        >
+          <div className="flex flex-col my-auto">
+            <h3 className="text-2xl font-semibold">{title}</h3>
+            <p className="text-lg text-slate-600 mt-2">{body}</p>
+          </div>
+          <Lottie
+            animationData={lottieData}
+            loop={true}
+            // style={{ width: animationWidth }}
+            className="mx-auto w-48"
+          />
+        </div>
+      </Slide>
+    </Fade>
   );
 };
 export default SingleBenefit;
