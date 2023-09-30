96d0ce6b220208672b7b227b7889bb42e720360b
JanGospodarek <jan.gospo@onet.pl>
2023-09-30 11:07:06 +0200

    ddd

--- components/main/landing/SingleBenefit.tsx
+++ components/main/landing/SingleBenefit.tsx
@@ -10,26 +10,32 @@ const SingleBenefit = (props: {
 }) => {
   const { title, body, placementClass, lottieData, animationWidth } = props;
   return (
-    <Fade>
-      <Slide direction="up">
-        <div
-          className={`flex sm:mx-16 lg:mx-0 flex-col md:flex-row lg:w-1/2  my-4 rounded-xl shadow-2xl text-center p-6 h-auto sm:h-56 ${
-            placementClass ? placementClass : ""
-          }`}
-        >
-          <div className="flex flex-col my-auto">
-            <h3 className="text-2xl font-semibold">{title}</h3>
-            <p className="text-lg text-slate-600 mt-2">{body}</p>
+    <div
+      className={`sm:mx-16 lg:mx-0 lg:w-1/2  my-4  h-auto sm:h-56 ${
+        placementClass ? placementClass : ""
+      }`}
+    >
+      <Fade>
+        <Slide direction="up">
+          <div
+            className={`flex flex-col md:flex-row  rounded-xl shadow-2xl text-center p-6  ${
+              placementClass ? placementClass : ""
+            }`}
+          >
+            <div className="flex flex-col my-auto">
+              <h3 className="text-2xl font-semibold">{title}</h3>
+              <p className="text-lg text-slate-600 mt-2">{body}</p>
+            </div>
+            <Lottie
+              animationData={lottieData}
+              loop={true}
+              // style={{ width: animationWidth }}
+              className="mx-auto w-48"
+            />
           </div>
-          <Lottie
-            animationData={lottieData}
-            loop={true}
-            // style={{ width: animationWidth }}
-            className="mx-auto w-48"
-          />
-        </div>
-      </Slide>
-    </Fade>
+        </Slide>
+      </Fade>
+    </div>
   );
 };
 export default SingleBenefit;
